f4e7062e8ba4b85f18a4b43b0403daf278f4d94a
Christian--1980 <ch.gpunkt@gmail.com>
2025-04-04 15:49:25 +0200

    make code more clear

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -26,17 +26,19 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
         X.at<double>(2, 0) = it1->z;
         X.at<double>(3, 0) = 1;
 
-        // project Lidar point into camera
+        // project Lidar point into camera like in excercise
         Y = P_rect_xx * R_rect_xx * RT * X;
         cv::Point pt;
+        
         // pixel coordinates
         pt.x = Y.at<double>(0, 0) / Y.at<double>(2, 0); 
         pt.y = Y.at<double>(1, 0) / Y.at<double>(2, 0); 
 
-        vector<vector<BoundingBox>::iterator> enclosingBoxes; // pointers to all bounding boxes which enclose the current Lidar point
+        vector<vector<BoundingBox>::iterator> enclosingBoxes; // pointers all bounding boxes incl. points
+        
         for (vector<BoundingBox>::iterator it2 = boundingBoxes.begin(); it2 != boundingBoxes.end(); ++it2)
         {
-            // shrinked box
+            // shrinked box to reduce noisy data
             cv::Rect shrinkedBox;
 
             shrinkedBox.x = (*it2).roi.x + shrinkFactor * (*it2).roi.width / 2.0;
@@ -50,16 +52,15 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
                 enclosingBoxes.push_back(it2);
             }
 
-        } // eof loop over all bounding boxes
+        }
 
         // check wether point has been enclosed by one or by multiple boxes
         if (enclosingBoxes.size() == 1)
         { 
-            // add Lidar point to bounding box
             enclosingBoxes[0]->lidarPoints.push_back(*it1);
         }
 
-    } // eof loop over all Lidar points
+    }
 }
 
 /* 
