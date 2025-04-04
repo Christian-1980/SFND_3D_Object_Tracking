ab8cc8031b110514649c88f0f43a8fe7c2430f82
Christian--1980 <ch.gpunkt@gmail.com>
2025-04-04 15:59:08 +0200

    added KPTMatches and clear code

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -140,49 +140,34 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
 // associate a given bounding box with the keypoints it contains
 void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, std::vector<cv::DMatch> &kptMatches)
 {
-    // loop over all Lidar points and associate them to a 2D bounding box
-    cv::Mat X(4, 1, cv::DataType<double>::type);
-    cv::Mat Y(3, 1, cv::DataType<double>::type);
+    double sum_distance = 0;
 
-    for (auto it1 = lidarPoints.begin(); it1 != lidarPoints.end(); ++it1)
-    {
-        // assemble vector for matrix-vector-multiplication
-        X.at<double>(0, 0) = it1->x;
-        X.at<double>(1, 0) = it1->y;
-        X.at<double>(2, 0) = it1->z;
-        X.at<double>(3, 0) = 1;
+    // matrix to store the matches in the Dmatrix
+    std::vector<cv::DMatch> matches;
 
-        // project Lidar point into camera chip system
-        Y = P_rect_xx * R_rect_xx * RT * X;
-        cv::Point pt;
-        pt.x = Y.at<double>(0, 0) / Y.at<double>(0, 2); // pixel coordinates
-        pt.y = Y.at<double>(1, 0) / Y.at<double>(0, 2);
+    // Find the mean distance between all matched keypoints
+    for(auto it = kptMatches.begin(); it != kptMatches.end(); ++it) {
+        cv::KeyPoint kpCurr = kptsCurr.at(it->trainIdx);
+        cv::KeyPoint kpPrev = kptsPrev.at(it->queryIdx);
 
-        vector<vector<BoundingBox>::iterator> enclosingBoxes; // pointers to all bounding boxes which enclose the current Lidar point
-        for (vector<BoundingBox>::iterator it2 = boundingBoxes.begin(); it2 != boundingBoxes.end(); ++it2)
-        {
-            // shrink current bounding box slightly to avoid having too many outlier points around the edges
-            cv::Rect smallerBox;
-            smallerBox.x = (*it2).roi.x + shrinkFactor * (*it2).roi.width / 2.0;
-            smallerBox.y = (*it2).roi.y + shrinkFactor * (*it2).roi.height / 2.0;
-            smallerBox.width = (*it2).roi.width * (1 - shrinkFactor);
-            smallerBox.height = (*it2).roi.height * (1 - shrinkFactor);
-
-            // check wether point is within current bounding box
-            if (smallerBox.contains(pt))
-            {
-                enclosingBoxes.push_back(it2);
-            }
-
-        } // eof loop over all bounding boxes
-
-        // check wether point has been enclosed by one or by multiple boxes
-        if (enclosingBoxes.size() == 1)
-        { 
-            // add Lidar point to bounding box
-            enclosingBoxes[0]->lidarPoints.push_back(*it1);
+        if(boundingBox.roi.contains(kpCurr.pt)) {
+            matches.push_back(*it);
+            sum_distance += cv::norm(kpCurr.pt - kpPrev.pt);
         }
+    }
 
+    // Find the threshold distance
+    double distMean = sum_distance / matches.size();
+    double threshold = distMean * 0.7;
+
+    // Find the matches that are within the threshold distance to filter out outliers
+    for(auto it = matches.begin(); it != matches.end(); ++it) {
+        cv::KeyPoint kpCurr = kptsCurr.at(it->trainIdx);
+        cv::KeyPoint kpPrev = kptsPrev.at(it->queryIdx);
+
+        if(cv::norm(kpCurr.pt - kpPrev.pt) < threshold) {
+            boundingBox.kptMatches.push_back(*it);
+        }
     }
 }
 
