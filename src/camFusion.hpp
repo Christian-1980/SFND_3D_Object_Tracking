9576a9a3f8e9923db15102a8ed008df1cd28edc5
Christian--1980 <ch.gpunkt@gmail.com>
2025-04-07 22:42:25 +0200

    small but necessary adjustmetn : rem visimg

--- src/camFusion.hpp
+++ src/camFusion.hpp
@@ -7,6 +7,7 @@
 #include <opencv2/core.hpp>
 #include "dataStructures.h"
 
+using namespace std;
 
 void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<LidarPoint> &lidarPoints, float shrinkFactor, cv::Mat &P_rect_xx, cv::Mat &R_rect_xx, cv::Mat &RT);
 void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, std::vector<cv::DMatch> &kptMatches);
@@ -15,7 +16,7 @@ void matchBoundingBoxes(std::vector<cv::DMatch> &matches, std::map<int, int> &bb
 void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, cv::Size imageSize, bool bWait=true);
 
 void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr,
-                      std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC, cv::Mat *visImg=nullptr);
+                      std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC);
 void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
                      std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC);                  
 #endif /* camFusion_hpp */
